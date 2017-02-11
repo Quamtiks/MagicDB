412571230bf8dba59cd672c27816a4c7bad30ac5
quamtiks <quamtiks@gmail.com>
2017-02-11 13:39:53 -0600

    Segunda consulta, la lista de Tickets por paises centroamericanos

---
+++ Consultas/consulta2.sql
@@ -0,0 +1,14 @@
+#2 Secgunda consulta, la lista de Tickets por paises centroamericanos
+# No se encontraron resultados por lo que 0 estan asociados
+USE `DATABASE`; 
+SELECT t.tickets, org.country 
+FROM swusers as u, swtickets as t, swuserorganizations as org
+WHERE u.userid = t.userid 
+AND u.userorganizationid = org.userorganizationid 
+AND org.country = 'Costa Rica'
+AND org.country = 'Panama'
+AND org.country = 'Nicaragua'
+AND org.country = 'El Salvador'
+AND org.country = 'Belice'
+AND org.country = 'Guatemala'
+AND org.country = 'Honduras';
