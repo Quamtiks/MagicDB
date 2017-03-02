6e1eaf6642b8e7502ea1de568fc7c7c32cf8f373
Quamtiks <kentarias@hotmail.com>
2017-03-01 22:53:40 -0600

    Archivo erroneo

--- Consultas/consulta1.sql
+++
@@ -1,5 +0,0 @@
-#1 Primer consulta, obtener la lista de direcciones IP relacionadas con el gobierno nicaraguense.
-#No se encuentran resultados
-USE `DATABASE`;
-SELECT cities.country ,blocks.ipfrom , blocks.ipto FROM swgeoipcities as cities ,swgeoipcityblocks1 as blocks
-WHERE blocks.blockid = cities.blockid and cities.country= 'NI';
